86febce7d3ee811013f435edd2104078e57d8f69
a.v.gurin <a.v.gurin@nefrosovet.ru>
2020-06-22 14:58:24 +0300

    создать Vue приложение с кнопкой-счетчиком

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -1,4 +1,17 @@
 import Vue from '/vendor/vue.esm.browser.js';
 
-// const app = ...
-// Рекомендуется использовать МЕТОД в качестве обработчика события
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    counter: 0,
+  },
+
+  methods: {
+    onBtnClick() {
+      this.counter += 1;
+    },
+  },
+
+});
+
