a6276be82500ed53ae204f50f0a1860ec3a82854
a.v.gurin <a.v.gurin@nefrosovet.ru>
2020-07-06 15:48:04 +0300

    компонент CounterButton

--- 2-module/1-task/CounterButton.js
+++ 2-module/1-task/CounterButton.js
@@ -1,21 +1,17 @@
 export const CounterButton = {
-  // Шаблон потребуется отредактировать
   template: '<button type="button" @click="handlerClick">{{ count }}</button>',
 
-  // Компонент должен иметь пропс
   props: {
     count: {
       type: Number,
       default: () => 0,
     },
   },
-  // Компонент должен иметь модель
   model: {
     prop: 'count',
     event: 'increment',
   },
 
-  // Шаблон лучше иметь максимально простым, а логику выносить в методы
   methods: {
     handlerClick() {
       let count = this.count + 1;
